93d114afe5d69f67aa378f16757ddce06d77ef16
Rohmad Kadarwanto <55898570+pakdherahmad@users.noreply.github.com>
2023-12-29 12:45:59 +0700

    Update serverTransaksi.js

--- serverTransaksi.js
+++ serverTransaksi.js
@@ -5,6 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const EntitasRoutes_1 = __importDefault(require("./routes/EntitasRoutes"));
 const JenisTransaksiRoutes_1 = __importDefault(require("./routes/JenisTransaksiRoutes"));
 const AkunKeuanganRoutes_1 = __importDefault(require("./routes/AkunKeuanganRoutes"));
@@ -13,9 +15,17 @@ const TransaksiDetailRoutes_1 = __importDefault(require("./routes/TransaksiDetai
 const SantriRoutes_1 = __importDefault(require("./routes/SantriRoutes"));
 const WaliSantriRoutes_1 = __importDefault(require("./routes/WaliSantriRoutes"));
 const NeracaRoutes_1 = __importDefault(require("./routes/NeracaRoutes"));
+const transaksi_1 = __importDefault(require("./routes/transaksi"));
 const app = (0, express_1.default)();
 const port = 3000;
 // Middleware and other configurations
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use((0, cors_1.default)());
+app.set('view engine', 'ejs');
+// Static files
+app.use(express_1.default.static('public'));
+app.use('/', transaksi_1.default);
 // Routes
 app.use('/api', EntitasRoutes_1.default);
 app.use('/api', JenisTransaksiRoutes_1.default);
